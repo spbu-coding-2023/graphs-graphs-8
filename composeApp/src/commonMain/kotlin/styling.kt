4029c299eb487e1e9dd59603665d0dcb824998b3
Gleb Nasretdinov <gleb.romashkov@gmail.com>
2024-05-08 03:30:48 +0300

    feat: add styles

---
+++ composeApp/src/commonMain/kotlin/styling.kt
@@ -0,0 +1,9 @@
+package visualizer.styling
+
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.TextStyle
+
+
+val defaultStyle = TextStyle(fontSize = 28.sp)
+
+val bigStyle = TextStyle(fontSize = 50.sp)
