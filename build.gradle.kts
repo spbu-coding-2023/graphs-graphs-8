eadadb9771482b41e534f6b7c6c9a88982c89179
Gleb Nasretdinov <gleb.romashkov@gmail.com>
2024-05-17 23:07:52 +0300

    Migrate project from multiplatform to desktop

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,40 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.composeMultiplatform) apply false
-    alias(libs.plugins.composeCompiler) apply false
-    id("org.jetbrains.kotlinx.kover") version "0.7.6"
+    kotlin("jvm") version "1.9.22"
+    id("org.jetbrains.compose")
+    id("org.jetbrains.kotlinx.kover") version "0.8.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
 }
 
-dependencies{
-    kover(project(":composeApp"))
-    kover(project(":lib"))
+group = "visualizer"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    // Note, if you develop a library, you should use compose.desktop.common.
+    // compose.desktop.currentOs should be used in launcher-sourceSet
+    // (in a separate module for demo project and in testMain).
+    // With compose.desktop.common you will also lose @Preview functionality
+    implementation(compose.desktop.currentOs)
+    val nav_version = "2.8.0-alpha02"
+    implementation("org.jetbrains.androidx.navigation:navigation-compose:$nav_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "GraphVisualizer"
+            packageVersion = "1.0.0"
+        }
+    }
 }
--- composeApp/src/commonMain/kotlin/App.kt
+++
@@ -1,9 +0,0 @@
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.*
-
-@Composable
-fun App(){
-    MaterialTheme(){
-        Navigation()
-    }
-}
