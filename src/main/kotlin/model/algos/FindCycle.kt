a56d80d566dc35f68cca8688dd814ee2205c2c61
Azamat Ishbaev <azamatigruha@gmail.com>
2024-05-25 09:04:12 +0300

    feat: implement new algorithm for find cycle

--- src/main/kotlin/model/algos/FindCycle.kt
+++ src/main/kotlin/model/algos/FindCycle.kt
@@ -1,46 +1,82 @@
 package model.algos
 
-import model.graph.UndirectedGraph
 import model.graph.edges.Edge
 
 object FindCycle {
-    private fun <V> findCycleUtil(
-        graph: UndirectedGraph<V>,
-        startVertex: V,
-        visited: MutableMap<V, Boolean>,
-        parent: V?,
-        path: MutableList<Edge<V>>
-    ): List<Edge<V>>? {
-        visited[startVertex] = true
+    fun <V> findCycles(graph: MutableMap<V, MutableList<Edge<V>>>, startVertex: V): List<List<V>> {
+        val blockedSet = mutableSetOf<V>()
+        val blockedMap = mutableMapOf<V, MutableSet<V>>()
+        val stack = mutableListOf<V>()
+        val preResult = mutableListOf<List<V>>()
+        val sccs = StrongConnections<V>()
+        val sccResult = sccs.findStrongConnections(graph)
 
-        graph.edgesOf(startVertex).forEach { edge ->
-            val next = if (edge.from == startVertex) edge.to else edge.from
-            if (visited[next] != true) {
-                path.add(edge)
-                val cyclePath = findCycleUtil(graph, next, visited, startVertex, path)
-                if (cyclePath != null) {
-                    return cyclePath
-                }
-                path.removeAt(path.size - 1)
-            } else if (parent != next) {
-                path.add(edge)
-                return path.dropWhile { it.from != next && it.to != next }.toList()
+        for (subGraph in sccResult) {
+            if (subGraph.size > 1) {
+                val startNode = subGraph.first()
+                findCyclesInSCC(startNode, startNode, graph, blockedSet, blockedMap, stack, preResult)
+                blockedSet.clear()
+                blockedMap.clear()
             }
         }
-        return null
+
+        val result = mutableListOf<List<V>>()
+        for (res in preResult){
+            if(res.contains(startVertex)){
+                result.add(res)
+            }
+        }
+        return result
     }
 
-    fun <V> findCycle(graph: UndirectedGraph<V>, start: V): List<Edge<V>>? {
-        val visited = mutableMapOf<V, Boolean>()
-        val path = mutableListOf<Edge<V>>()
+    private fun <V> findCyclesInSCC(
+        start: V,
+        current: V,
+        graph: MutableMap<V, MutableList<Edge<V>>>,
+        blockedSet: MutableSet<V>,
+        blockedMap: MutableMap<V, MutableSet<V>>,
+        stack: MutableList<V>,
+        result: MutableList<List<V>>
+    ): Boolean {
+        var foundCycle = false
+        stack.add(current)
+        blockedSet.add(current)
 
-        if (visited[start] != true) {
-            val cyclePath = findCycleUtil(graph, start, visited, null, path)
-            if (cyclePath != null) {
-                return cyclePath
+        for (edge in graph[current] ?: mutableListOf()) {
+            val neighbor = edge.to
+            if (neighbor == start) {
+                val cycle = stack.toList()
+                result.add(cycle)
+                foundCycle = true
+            } else if (neighbor !in blockedSet) {
+                if (findCyclesInSCC(start, neighbor, graph, blockedSet, blockedMap, stack, result)) {
+                    foundCycle = true
+                }
             }
         }
 
-        return null
+        if (foundCycle) {
+            unblock(current, blockedSet, blockedMap)
+        } else {
+            for (edge in graph[current] ?: mutableListOf()) {
+                val neighbor = edge.to
+                blockedMap.computeIfAbsent(neighbor) { mutableSetOf() }.add(current)
+            }
+        }
+
+        stack.removeAt(stack.size - 1)
+        return foundCycle
+    }
+
+    private fun <V> unblock(node: V, blockedSet: MutableSet<V>, blockedMap: MutableMap<V, MutableSet<V>>) {
+        val stack = mutableListOf(node)
+        while (stack.isNotEmpty()) {
+            val current = stack.removeAt(stack.size - 1)
+            if (current in blockedSet) {
+                blockedSet.remove(current)
+                blockedMap[current]?.forEach { stack.add(it) }
+                blockedMap.remove(current)
+            }
+        }
     }
 }
