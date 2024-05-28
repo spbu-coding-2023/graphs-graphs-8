71e1f6ce827e2e1c573f32a7d3bbb5f5acfc400f
Gleb Nasretdinov <gleb.romashkov@gmail.com>
2024-05-28 22:45:46 +0300

    Fix: fix device pixels dependencies (was not correct view on macos)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,44 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPosition
-import androidx.compose.ui.window.WindowState
-import androidx.compose.ui.window.application
+import androidx.compose.ui.window.*
 import java.awt.Dimension
+import java.awt.GraphicsEnvironment
 
-val width = 1920
-val height = 1080
+val width = try {
+    GraphicsEnvironment.getLocalGraphicsEnvironment().maximumWindowBounds.width
+} catch (e: Exception) {
+    100
+}
+val height = try {
+    GraphicsEnvironment.getLocalGraphicsEnvironment().maximumWindowBounds.height
+} catch (e: Exception) {
+    100
+}
 
-fun main() = application {
-    val state = WindowState(
-        width = width.dp,
-        height = height.dp,
-        position = WindowPosition(alignment = Alignment.Center),
-    )
-    Window(
-        state = state,
-        onCloseRequest = ::exitApplication,
-        title = "Graph Visualizer",
-    ) {
-        window.minimumSize = Dimension(100, 100)
-        App()
+fun main() {
+    application {
+        val state = WindowState(
+            width = width.dp,
+            height = height.dp,
+            position = WindowPosition(alignment = Alignment.Center),
+        )
+        Window(
+            state = state,
+            onCloseRequest = ::exitApplication,
+            title = "Graph Visualizer",
+        ) {
+            window.minimumSize = Dimension(100, 100)
+            App()
+        }
     }
 }
 
 @Composable
 fun App() {
+
     MaterialTheme() {
         Navigation()
     }
